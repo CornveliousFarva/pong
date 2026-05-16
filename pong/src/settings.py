5e6fcf45da4250aac0cd8344b00c65fbaaca1a99
Greg Schuman <greg.c.schuman@gmail.com>
2026-05-16 15:55:05 -0700

    Added settings for ball colors, paddle colors, and winning score.

---
+++ pong/src/settings.py
@@ -0,0 +1,23 @@
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 600
+FPS = 60
+
+WINNING_SCORE = 11
+
+
+PADDLE_COLORS = [
+    (255, 255, 255),  # White
+    (255, 0, 0),      # Red
+    (0, 120, 255),    # Blue
+    (0, 200, 120),    # Green
+    (180, 0, 255),    # Purple
+    (255, 215, 0),    # Gold
+]
+
+BALL_COLORS = [
+    (255, 230, 0),    # Tennis yellow
+    (255, 255, 255),  # White
+    (0, 255, 120),    # Neon green
+    (255, 120, 0),    # Orange
+    (255, 0, 180),    # Pink
+]
